f19c59cb6482e176d9d8aacfc1fb3e895f66109c
Victoria Fierce <victoria@carlaef.org>
2019-08-08 23:19:55 +0000

    sidebar: library_page: add a list of related library content

--- sidebar-library_page.php
+++ sidebar-library_page.php
@@ -6,6 +6,29 @@ if ( ( is_single() || is_page() ) && in_array( get_post_meta( get_queried_object
 if ( is_active_sidebar( 'library_page' ) || true ) : ?>
 	<div id="sidebar">
 		<?php dynamic_sidebar( 'library_page' ); ?>
+		<?php
+			$document_terms = get_the_terms(get_the_ID(), 'library_tag');
+			$terms = array();
+			foreach($document_terms as $term) {
+				$terms[] = $term->term_id;
+			}
+			$relatedQuery = new WP_Query(array(
+				'post_type' => 'any',
+				'tax_query' => array(array(
+					'taxonomy' => 'library_tag',
+					'terms' => $terms
+				))
+			));
+			if ($relatedQuery->have_posts()):?>
+				<h3>Related Library Content</h3>
+			<?php
+			endif;
+			while($relatedQuery->have_posts()):
+				$relatedQuery->the_post();
+				get_template_part('partials/library-card', get_post_type());
+			endwhile;
+			wp_reset_postdata();
+		?>
 	</div> <!-- end #sidebar -->
 <?php
 endif;
